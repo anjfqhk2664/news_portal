b5ba7ca6f4bf1fd078db7b532e761a5b71cf2b79
LSH <anjfqhk2664@naver.com>
2025-05-08 11:51:02 +0900

    first commit

---
+++ lib/presentation/app/router/app_router.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/widgets.dart';
+import 'package:go_router/go_router.dart';
+import 'package:news_portal/presentation/login/login_screen.dart';
+import 'package:news_portal/presentation/main_list/news_list_screen.dart';
+
+final appRouter = GoRouter(
+  initialLocation: AppRoute.login.path,
+  routes: [
+    GoRoute(
+      path: AppRoute.login.path,
+      name: AppRoute.newsList.name,
+      builder: (context, state) => LoginScreen(),
+    ),
+    GoRoute(
+      path: AppRoute.newsList.path,
+      name: AppRoute.newsList.name,
+      pageBuilder: (context, state) {
+        return CustomTransitionPage(
+          key: state.pageKey,
+          child: NewsListScreen(),
+          transitionsBuilder: (context, animation, secondaryAnimation, child) {
+            return FadeTransition(
+              opacity: CurveTween(
+                curve: Curves.easeInOutCirc,
+              ).animate(animation),
+              child: child,
+            );
+          },
+        );
+      },
+    ),
+  ],
+);
+
+enum AppRoute { login, newsList }
+
+extension AppRouteExtension on AppRoute {
+  String get path => '/$this';
+}
